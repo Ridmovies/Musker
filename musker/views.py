437992ab2fd3bb915a0c73b9566bfcc01ee0600b
Riddhub <unkownrid@gmail.com>
2023-07-20 13:28:49 +0300

    - test

--- musker/views.py
+++ musker/views.py
@@ -83,7 +83,6 @@ def profile(request, pk):
 
         # Post Form logic
         if request.method == "POST":
-            # Get current user
             current_user_profile = request.user.profile
             # Get form data
             action = request.POST['follow']
